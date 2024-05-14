072d17941b89a380546d44e23156087a398612fb
JoseArmandoRosales <jose_rosalesr@hotmail.com>
2024-05-14 16:15:29 -0700

    stack navigator added

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import ViewAllRentasScreen from './screens/viewAllRentasScreen'
+import ViewAltaRentasScreen from './screens/ViewAltaRentasScreen'
+
+const Stack = createNativeStackNavigator()
+
+function MyStack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='Rentas' component={ViewAllRentasScreen} />
+      <Stack.Screen name='Alta Renta - Buscar Cliente' component={ViewAltaRentasScreen} />
+
+    </Stack.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
+
   );
 }
 
